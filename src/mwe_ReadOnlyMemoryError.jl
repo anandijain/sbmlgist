5f06978d9ca60d19a06cd3ab8867ce3ea0b5dbd3
anand jain <anandj@uchicago.edu>
2021-04-28 16:35:10 -0700

    fix path

--- src/mwe_ReadOnlyMemoryError.jl
+++ src/mwe_ReadOnlyMemoryError.jl
@@ -3,7 +3,7 @@ using SBML_jll, Libdl
 sbml = (sym::Symbol) -> dlsym(SBML_jll.libsbml_handle, sym)
 const VPtr = Ptr{Cvoid}
 
-sbmlfile = joinpath("test", "reactionsystem_01.xml")  # Point to any SBML file
+sbmlfile = joinpath(@__DIR__, "reactionsystem_01.xml")  # Point to any SBML file
 doc = ccall(sbml(:readSBML), VPtr, (Cstring,), sbmlfile)   
 props = ccall(sbml(:ConversionProperties_create), VPtr, ())
 props = ccall(sbml(:ConversionProperties_addOption), VPtr, (Cstring,), "replaceReactions")
